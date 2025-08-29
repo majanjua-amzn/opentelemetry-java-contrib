3ce2665e35b18cbbfd5e58f8d56e7b557207e5e8
Mahad Janjua <majanjua@amazon.com>
2025-08-29 10:37:37 -0700

    Add AWS X-Ray Adaptive Sampling Support

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "1.48.0"
-val alphaVersion = "1.48.0-alpha"
+val stableVersion = "1.48.0-adot1"
+val alphaVersion = "1.48.0-alpha-adot1"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
